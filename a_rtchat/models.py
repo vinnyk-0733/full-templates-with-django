907c6659e090e69fb1016cf49cca042691a911aa
vinayak_0733 <vinayakumar464@gmail.com>
2025-10-09 08:47:10 +0530

    file sending done

--- a_rtchat/models.py
+++ a_rtchat/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 import shortuuid
+import os
+from PIL import Image
 
 class ChatGroup(models.Model):
-    group_name = models.CharField(max_length=128,unique=True,default=shortuuid.uuid)
+    group_name = models.CharField(max_length=128,unique=True,blank=True)
     groupchat_name = models.CharField(max_length=128, null=True,blank=True)
     admin = models.ForeignKey(User, related_name='groupchats',blank=True,null=True, on_delete=models.SET_NULL)
     user_online = models.ManyToManyField(User,related_name='onine_in_groups',blank=True)
@@ -13,15 +15,46 @@ class ChatGroup(models.Model):
     def __str__(self):
         return self.group_name
 
+    def save(self,*args,**kwargs):
+        if not self.group_name:
+            self.group_name = shortuuid.uuid()
+        super().save(*args,**kwargs)
+
 
 class GroupMessage(models.Model):
     group = models.ForeignKey(ChatGroup,related_name='chat_messages',on_delete=models.CASCADE)
     author = models.ForeignKey(User,on_delete=models.CASCADE)
-    body = models.CharField(max_length=300)
+    body = models.CharField(max_length=300,blank=True,null=True)
+    file = models.FileField(upload_to='files/', blank=True,null=True)
     created = models.DateTimeField(auto_now_add=True)
+    
+    @property
+    def filename(self):
+        if self.file:
+            return os.path.basename(self.file.name)
+        else:
+            return None
 
     def __str__(self):
-        return f'{self.author.username} : {self.body}'
+        if self.body:
+            return f'{self.author.username} : {self.body}'
+        elif self.file:
+            return f'{self.author.username} : {self.filename}'
 
     class Meta:
         ordering = ['-created']
+        
+    @property
+    def is_image(self):
+        # if self.filename.lower().endswith(('.png','.jpg','.jpeg','.gif','.svg','.webp',)):
+        #     return True
+        # else:
+        #     return False
+        
+        try:
+            image = Image.open(self.file)
+            image.verify()
+            return True
+        except:
+            return False
+
--- a_rtchat/views.py
+++ a_rtchat/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render,get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.http import Http404,HttpResponseNotAllowed
+from django.http import Http404, HttpResponse
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
 from .models import *
 from .forms import *
 
 @login_required
 def chat_view(request,chatroom_name = 'public-chat'):    
-    
     chat_group = get_object_or_404(ChatGroup, group_name = chatroom_name)
     chat_messages = chat_group.chat_messages.all()[:30]
     form = ChatMessageCreateForm()
@@ -147,3 +148,24 @@ def chatroom_leave_view(request, chatroom_name):
     chat_group.members.remove(request.user)
     messages.success(request, 'You left the Chat')
     return redirect('home')
+
+
+def chat_file_upload(request,chatroom_name):
+    chat_group = get_object_or_404(ChatGroup,group_name = chatroom_name)
+
+    if request.htmx and request.FILES:
+        file = request.FILES['file']
+        message = GroupMessage.objects.create(
+            file = file,
+            author = request.user,
+            group = chat_group,
+        )
+        channel_layer = get_channel_layer()
+        event = {
+            'type': 'message_handler',
+            'message_id': message.id,
+        }
+        async_to_sync(channel_layer.group_send)(
+            chatroom_name, event
+        )
+    return HttpResponse()
